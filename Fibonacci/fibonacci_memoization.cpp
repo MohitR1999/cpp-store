42a171d4067ebfe6fdee7a6cb5718743308af0f7
Mostafa Saeed Hashhash <mostafasaeed483@gmail.com>
2019-10-24 18:10:52 +0200

    Update fibonacci_memoization.cpp

--- Fibonacci/fibonacci_memoization.cpp
+++ Fibonacci/fibonacci_memoization.cpp
@@ -1,28 +1,25 @@
 #include <bits/stdc++.h>
-
+#define long long ll 
 using namespace std;
 
-long fibm(long n,vector<long> qb){
+ll fibm(ll n,vector<ll> qb){
     
-    if(n==1 ||n==0){
-    	return n;
-    }
-    if(qb[n]!=0){
+    if( n < 2 )
+    	return n; 
+    
+    if(qb[n]!=0)
     	return qb[n];
-    }
-    else{
-      int x=fibm(n-1,qb) +fibm(n-2,qb);
-      qb[n]=x;
-      return x;
+  
+    else
+    {
+      qb[n]=fibm(n-1,qb) +fibm(n-2,qb);
+      return qb[n];
     }
 }
 
 
-int main(){
-int n=10;
- long  res=fibm(n,vector<long> (n+1,0));
-cout<<res<<" ";
-
-cout<<endl;
-return 0;
+void main(){
+    int n=10;
+    ll  res = fibm(n,vector<ll> (n+1,0));
+    cout<<res<<" " << endl;
 }
