eb68b21498cb0fc9234712ab89645e35f9617b64
PhalakS <36873064+CO17345@users.noreply.github.com>
2019-10-28 00:28:26 +0530

    Updated the condition to check odd or even number

--- evenodd.c
+++ evenodd.c
@@ -5,7 +5,7 @@ int main()
     int x;
     printf("Enter a number: ");
     scanf("%d", &x);
-    if(x&1)
+    if(x%2!=0)
         printf("%d is ODD\n", x);
     else
         printf("%d is EVEN\n", x);
